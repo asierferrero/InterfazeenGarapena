a4808c74d34f639369132795816d470d8114f5c7
asierferrero <ferrero.asier@uni.eus>
2023-11-28 12:25:54 +0100

    wineshop bukatuta

--- Visual Studio/2. Erronka/Wineshop (Rest API)/WineShop/Controllers/HomeController.cs
+++ Visual Studio/2. Erronka/Wineshop (Rest API)/WineShop/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WineShop.Models;
+using WineShop.Services;
 
 namespace WineShop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IArdoaService _ardoaService;
+        public HomeController(ILogger<HomeController> logger, IArdoaService ardoaService)
         {
             _logger = logger;
+            _ardoaService = ardoaService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Ardoa> ardoaList = new List<Ardoa>();
+            ardoaList = await _ardoaService.GetArdoak();
+            return View(ardoaList.Where(a => a.Eskaintza == true));
         }
 
         public IActionResult Privacy()
