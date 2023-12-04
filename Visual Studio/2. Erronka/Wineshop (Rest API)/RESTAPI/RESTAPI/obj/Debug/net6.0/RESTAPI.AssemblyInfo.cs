13c724fa089692b361f19ffed4afe4cd9baebab0
asierferrero <ferrero.asier@uni.eus>
2023-12-04 09:57:28 +0100

    wineshop pertsonala bukatuta

--- Visual Studio/2. Erronka/Wineshop (Rest API)/RESTAPI/RESTAPI/obj/Debug/net6.0/RESTAPI.AssemblyInfo.cs
+++ Visual Studio/2. Erronka/Wineshop (Rest API)/RESTAPI/RESTAPI/obj/Debug/net6.0/RESTAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4808c74d34f639369132795816d470d8114f5c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RESTAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RESTAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Visual Studio/2. Erronka/Wineshop (Rest API)/WineShop/obj/Debug/net8.0/WineShop.AssemblyInfo.cs
+++ Visual Studio/2. Erronka/Wineshop (Rest API)/WineShop/obj/Debug/net8.0/WineShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4808c74d34f639369132795816d470d8114f5c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
