a4fe866ba5d22156d174b5e03b0766f306bb6d93
asierferrero <ferrero.asier@uni.eus>
2024-01-11 10:00:34 +0100

    wineshop changes done

---
+++ Visual Studio/2. Erronka/Wineshop index/WineShop/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*Saskira gehitzeko*/
+$(document).ready(function () {
+    $(".AddLink").click(function (e) {
+        e.preventDefault();
+        // Get the id from the link
+        var recordToAdd = $(this).attr("data-id");
+        var kantitatea = parseInt($(this).attr("data-kantitatea"));
+        var salneurria = parseFloat($(this).attr("data-salneurria"));
+        var guztira = parseFloat($("#cart-total").text());
+        if (recordToAdd !== '') {
+            // Perform the ajax post
+            $.post("/Saskia/SaskiaGehituAjax", { "id": recordToAdd, "kantitatea": kantitatea, "salneurria": salneurria, "guztira": guztira },
+                function (data) {
+                    // Successful requests get here
+                    // Update the page elements
+                    $("#item-count-" + recordToAdd).text(data.kantitatea.toString());
+                    $("#cart-total").text((data.guztira + data.salneurria).toString());
+                    $("#update-message").text(data.mezua);
+                });
+        }
+    });
+})
