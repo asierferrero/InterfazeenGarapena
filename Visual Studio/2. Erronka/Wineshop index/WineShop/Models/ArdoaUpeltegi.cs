f025214735946cf0c00f559b9f885d9efb89c9d0
asierferrero <ferrero.asier@uni.eus>
2024-01-16 14:08:15 +0100

    repository updated

--- Visual Studio/2. Erronka/Wineshop index/WineShop/Models/ArdoaUpeltegi.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WineShop.Models
-{
-    public class ArdoaUpeltegi
-    {
-        public int Id { get; set; }
-        public string Izena { get; set; }
-        public string UpeltegiIzena { get; set; }
-    }
-}
